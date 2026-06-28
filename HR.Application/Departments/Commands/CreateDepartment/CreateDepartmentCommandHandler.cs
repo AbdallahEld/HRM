f3958de43fc9c7d3b826d37404821e7b791d5d09
AbdallahEld <abdallaheldesoky301@gmail.com>
2026-06-28 15:13:02 +0300

    Handle Children Not Exceeding Parent Headcount

--- HR.Application/Departments/Commands/CreateDepartment/CreateDepartmentCommandHandler.cs
+++ HR.Application/Departments/Commands/CreateDepartment/CreateDepartmentCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using HR.Domain.Data.Entities;
+﻿using FluentValidation.Results;
+using HR.Domain.Data.Entities;
 using HR.Domain.UnitOfWork;
 using MediatR;
 using System;
@@ -12,6 +13,36 @@ namespace HR.Application.Departments.Commands.CreateDepartment
     {
         public async Task<int> Handle(CreateDepartmentCommand request, CancellationToken cancellationToken)
         {
+            if (request.ParentDepartmentId.HasValue)
+            {
+                var parentId = request.ParentDepartmentId.Value;
+
+                var parentDepartment = await unitOfWork._DepartmentRepository.GetByIdAsync(parentId);
+                if (parentDepartment == null)
+                {
+                    throw new FluentValidation.ValidationException(new List<ValidationFailure>
+                    {
+                        new ValidationFailure("ParentDepartmentId" , "Parent department does not exist.")
+                    });
+                }
+
+                var currentChildren = await unitOfWork._DepartmentRepository
+                                                      .FindAsync(d => d.ParentDepartmentId == parentId);
+
+                var totalExistingHeadCount = currentChildren.Sum(d => d.HeadCount);
+
+                if ((totalExistingHeadCount + request.HeadCount) > parentDepartment.HeadCount)
+                {
+                    var availableCapacity = parentDepartment.HeadCount - totalExistingHeadCount;
+                    throw new FluentValidation.ValidationException(new List<ValidationFailure>
+                    {
+                        new ValidationFailure("HeadCount", $"Cannot exceed parent department headcount. Available capacity is {availableCapacity}.")
+                    });
+                }
+
+
+            }
+
             var department = new Department
             {
                 Name = request.Name,
--- HR.Application/Departments/Commands/CreateDepartment/CreateDepartmentCommandValidator.cs
+++ HR.Application/Departments/Commands/CreateDepartment/CreateDepartmentCommandValidator.cs
@@ -11,7 +11,7 @@ namespace HR.Application.Departments.Commands.CreateDepartment
         {
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Department name is required")
-                .MinimumLength(100).WithMessage("Department name cannot exceed 100 characters.");
+                .MaximumLength(100).WithMessage("Department name cannot exceed 100 characters.");
 
             RuleFor(x => x.ManagerId)
                 .GreaterThan(0).When(x => x.ManagerId.HasValue).WithMessage("Manager ID must be greater than 0.");
